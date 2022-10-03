5ef23dcca67c6643cc73a138b7ff6fcda8c88f40
DeveloperNeil <neil.mcnaught@secomind.ai>
2022-10-03 15:05:46 +0100

    Initial Commit

---
+++ CodeReload/CodeReloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReloadApp.swift
+//  CodeReload
+//
+//  Created by Neil McNaught on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeReloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeReload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeReload
+//
+//  Created by Neil McNaught on 03/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
